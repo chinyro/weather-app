c0e34ec29ee40b88c5cb15459c027999bca28028
sydrob <55166840+sydrobhez@users.noreply.github.com>
2024-01-04 18:14:24 -0600

    updated styles and icon API

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function updateWeather(response) {
   let windSpeedElement = document.querySelector('#wind-speed');
   let timeElement = document.querySelector('#current-date');
   let date = new Date(response.data.time * 1000);
-
-  console.log(response.data);
+  let iconElement = document.querySelector('#icon');
 
   currentCity.innerHTML = response.data.city;
-
   timeElement.innerHTML = formateDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img class="current-temperature-icon" src="${response.data.condition.icon_url}" />`;
 }
+
 // Format Date
 function formateDate(date) {
   let minutes = date.getMinutes();
@@ -37,7 +37,6 @@ function formateDate(date) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   if (hours < 10) {
     hours = `0${hours}`;
   }
